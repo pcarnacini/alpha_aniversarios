f8e72ac59eca8593e1f1746b84ca8b5b82ec3e46
Pedro Carnacini <pedrohenriquecarnacini@gmail.com>
2025-01-06 11:46:34 -0300

    Agendador

---
+++ appaniversário.bat
@@ -0,0 +1,3 @@
+@echo off
+C:/Users/Usuário/Documents/Code/alpha_aniversarios/.venv/Scripts/python.exe C:/Users/Usuário/Documents/Code/alpha_aniversarios/appaniversario.py >> C:/Users/Usuário/Documents/Code/alpha_aniversarios/log.txt 2>&1
+pause
