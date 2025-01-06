4fc67806b442462f07d1923d4481117d2dda01f4
Pedro Carnacini <pedrohenriquecarnacini@gmail.com>
2025-01-06 13:11:45 -0300

    Bat para fazer automático diariamente

--- appaniversário.bat
+++ appaniversário.bat
@@ -1,3 +1,3 @@
 @echo off
-C:/Users/Usuário/Documents/Code/alpha_aniversarios/.venv/Scripts/python.exe C:/Users/Usuário/Documents/Code/alpha_aniversarios/appaniversario.py >> C:/Users/Usuário/Documents/Code/alpha_aniversarios/log.txt 2>&1
-pause
+call .venv\Scripts\activate
+python appaniversario.py
